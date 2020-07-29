9b6fc694ecbabc81920b59f8ab09257d4ed347fd
libarry <870390541@qq.com>
2020-07-29 23:43:52 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers{
+	public static void main(String[] args){
+		int x = 0;
+		for(int i = 1;i < 11;i++){
+			System.out.print(x + " ");
+			x = x + i;
+		}
+		System.out.println();
+	}
+}
+
